8d499840b234e379b595b60041fe5a7bad57c7c5
Niels Laurens <nlaurens@gmail.com>
2014-12-28 23:00:16 +0100

    Updates example to include remote DST

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 python backup.py /home/niels/src/ /home/niels/dst/ --exclude tmp
+python backup.py /home/niels/src/ user@host:/mnt/backups/dst/ --exclude tmp
+
 python backup.py /home/niels/src2/ /home/niels/dst2/ --exclude tmp
+python backup.py /home/niels/src2/ user@host:/mnt/backups/dst2/ --exclude tmp
